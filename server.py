25e216c3093ef8246c09ae8c5ec0158228246cb4
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2018-12-02 19:39:38 +0100

    Added an endpoint for downloading the resume

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(os.path.realpath(__file__)), "build/static"), template_folder=os.path.join(os.path.dirname(os.path.realpath(__file__)), "build"))
@@ -6,6 +6,10 @@ app = Flask(__name__, static_folder=os.path.join(os.path.dirname(os.path.realpat
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/resume", methods=['GET'])
+def download():
+    return send_from_directory(directory="./uploads/", filename="resume.pdf"), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
