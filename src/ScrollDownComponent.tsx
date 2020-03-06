488387557d76f72667c83732e74fa656e56e2fec
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2020-03-06 11:23:06 +0100

    Make scroll arrow not show in print

--- src/ScrollDownComponent.tsx
+++ src/ScrollDownComponent.tsx
@@ -5,7 +5,7 @@ import down from './assets/down-arrow.png';
 class ScrollDownComponent extends React.Component<{}, {}> {
   public render() {
     return (
-      <div className={'Scroll-icon'}>
+      <div className={'Scroll-icon no-print'}>
         <div className={'Scroll-text'}>Scroll down</div>
         <img className={'Icon-large Icon-bounce'} src={down} />
       </div>
